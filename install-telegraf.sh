2ba4a21417dfd860a31578d80ad26c390841e063
schiz0phr3ne <22858496+schiz0phr3ne@users.noreply.github.com>
2022-05-28 21:35:32 +0200

    Update install-telegraf.sh

--- install-telegraf.sh
+++ install-telegraf.sh
@@ -1,60 +1,14 @@
 #!/usr/bin/env bash
-YW=`echo "\033[33m"`
-RD=`echo "\033[01;31m"`
-BL=`echo "\033[36m"`
-GN=`echo "\033[1;92m"`
-CL=`echo "\033[m"`
-RETRY_NUM=10
-RETRY_EVERY=3
-NUM=$RETRY_NUM
-CM="${GN}✓${CL}"
-CROSS="${RD}✗${CL}"
-BFR="\\r\\033[K"
-HOLD="-"
-set -o errexit
-set -o errtrace
-set -o nounset
-set -o pipefail
-shopt -s expand_aliases
-alias die='EXIT=$? LINE=$LINENO error_exit'
-trap die ERR
+apt-get install -y lsb-base
+apt-get install -y lsb-release
+apt-get install -y gnupg2
 
-function error_exit() {
-  trap - ERR
-  local reason="Unknown failure occured."
-  local msg="${1:-$reason}"
-  local flag="${RD}‼ ERROR ${CL}$EXIT@$LINE"
-  echo -e "$flag $msg" 1>&2
-  exit $EXIT
-}
+wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add -
+echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/influxdb.list
 
-function msg_info() {
-    local msg="$1"
-    echo -ne " ${HOLD} ${YW}${msg}..."
-}
-
-function msg_ok() {
-    local msg="$1"
-    echo -e "${BFR} ${CM} ${GN}${msg}${CL}"
-}
-
-msg_info "Installing Dependencies"
-apt-get install -y lsb-base &>/dev/null
-apt-get install -y lsb-release &>/dev/null
-apt-get install -y gnupg2 &>/dev/null
-msg_ok "Installed Dependencies"
-
-msg_info "Setting up InfluxDB Repository"
-wget -qO- https://repos.influxdata.com/influxdb.key | sudo apt-key add - &>/dev/null
-echo "deb https://repos.influxdata.com/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/influxdb.list &>/dev/null
-msg_ok "Set up InfluxDB Repository"
-
-msg_info "Installing Telegraf"
-apt-get install -y telegraf &>/dev/null
+apt-get install -y telegraf
 msg_ok "Installed Telegraf"
 
-msg_info "Cleaning up"
-apt-get autoremove >/dev/null
-apt-get autoclean >/dev/null
+apt-get autoremove
+apt-get autoclean
 rm -rf /var/{cache,log}/* /var/lib/apt/lists/*
-msg_ok "Cleaned"
